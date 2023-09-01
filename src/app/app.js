3d763657d85f3483f1d862aea3be994211d25170
alexsuslin <alexsuslin@inbox.ru>
2023-09-01 13:47:24 +0300

    add flight.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,14 @@
 
 import s from "./app.module.css";
 import {Filters} from "../filtres/filters";
-import {Flight} from "../flight/flight";
+import {Flights} from "../flight/flights";
 import React from "react";
 
 function App() {
     return (
         <div className={s.app}>
             <Filters/>
-            <Flight/>
+            <Flights/>
         </div>
     );
 }
---
+++ src/flight/flight.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Flight = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
