2578ca053dc3575fdaca0f9730fda825785e6aa4
alexsuslin <alexsuslin@inbox.ru>
2023-09-03 09:42:37 +0300

    add redux

--- src/filtres/sort/sort.js
+++ src/filtres/sort/sort.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from '../filters.module.css';
+import data from "../../data/flights.json";
 
-export const Sort = () => {
+export const Sort = ({ onSort }) => {
+    const flights = data.result.flights;
+
+
+
     return (
         <div>
             <h4>Сортировать:</h4>
             <div>
-                <input type="radio" id="ascending" name="sorting" className={s.radioInput} value="ascending" />
+                <input
+                    type="radio"
+                    id="ascending"
+                    name="sorting"
+                    className={s.radioInput}
+                    value="ascending"
+
+                />
                 <label htmlFor="ascending" className={s.radioLabel}>
                     по возрастанию цены
                 </label>
             </div>
             <div>
-                <input type="radio" id="descending" name="sorting" className={s.radioInput} value="descending" />
+                <input
+                    type="radio"
+                    id="descending"
+                    name="sorting"
+                    className={s.radioInput}
+                    value="descending"
+
+                />
                 <label htmlFor="descending" className={s.radioLabel}>
                     по убыванию цены
                 </label>
             </div>
             <div>
-                <input type="radio" id="arrivalTime" name="sorting" className={s.radioInput} value="arrivalTime" />
+                <input
+                    type="radio"
+                    id="arrivalTime"
+                    name="sorting"
+                    className={s.radioInput}
+                    value="arrivalTime"
+
+                />
                 <label htmlFor="arrivalTime" className={s.radioLabel}>
                     по времени в пути
                 </label>
@@ -26,5 +52,6 @@ export const Sort = () => {
         </div>
     );
 };
+
 
 
